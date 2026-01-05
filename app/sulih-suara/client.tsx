825261b02df8eeb7337db84647b93ce7d12a8e46
azizkunbaru-blip <azizkunbaru@gmail.com>
2026-01-05 22:53:37 +0700

    Update client.tsx

--- app/sulih-suara/client.tsx
+++ app/sulih-suara/client.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState } from 'react';
 import { DramaSection } from '@/components/sections/drama-section';
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
